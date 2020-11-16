afe7c0e70c028efc704c54968865a53f8ac9f47d
Julio Suriano <jjsuriano@gmail.com>
2020-11-16 13:57:21 -0600

    added dynamic year to footer

--- static/js/load.js
+++ static/js/load.js
@@ -1,3 +1,6 @@
-window.addEventListener('load', function(event) {
-    document.querySelector("body").className = "";
+window.addEventListener("load", function (event) {
+  document.querySelector("body").className = "";
 });
+
+const now = new Date();
+document.querySelector("#year").innerHTML = now.getFullYear();
